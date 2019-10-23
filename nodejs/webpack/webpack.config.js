2cffa36f115f3602ab5ee12a26995663a4b731df
yyj4399 <admin@yyj4399.com>
2019-10-23 23:08:55 +0800

    add

--- nodejs/webpack/webpack.config.js
+++ nodejs/webpack/webpack.config.js
@@ -3,7 +3,18 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
   }
 };
